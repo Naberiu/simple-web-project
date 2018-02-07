f2d1aba3738a28f64ce03cadd0da752bea814acb
Miroslav Koštiál <miroslav.kostial@sos-jh.cz>
2018-02-07 09:33:49 +0100

    Add thymeleaf

--- src/main/java/cz/sosjh/web/controllers/IndexController.java
+++ src/main/java/cz/sosjh/web/controllers/IndexController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class IndexController {
 
     @GetMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
-    public ResponseEntity<String> index(@RequestParam(name = "myName", defaultValue = "World") String myName) {
-        return ResponseEntity.ok("<html><body><h1>Hello " + myName + "</h1></body></html>");
+    public String index(@RequestParam(name = "myName", defaultValue = "World") String myName) {
+        return "index";
     }
 }
 
